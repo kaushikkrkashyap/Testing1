8e55537b8dcb3d2a8ea5ed5d9a86ffb7434c29ac
LENOVO INDIA <LENOVO INDIA@DESKTOP-J70LVDP>
2020-05-01 20:09:05 +0530

    First Commit

---
+++ PageObjectModel/src/com/abc/MagentoMain/MagentoTest.java
@@ -0,0 +1,31 @@
+package com.abc.MagentoMain;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.abc.MagentoObjects.Home;
+import com.abc.MagentoObjects.Login;
+import com.abc.MagentoObjects.Welcome;
+
+public class MagentoTest {
+
+	public static void main(String[] args) {
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.get("https://www.magento.com");
+		
+		Welcome w=new Welcome(driver);
+		w.clickOnMyAccount();
+		Login l=new Login(driver);
+		l.typeEmail("ihassan395@gmail.com");
+		l.typePwd("Welcome@123");
+		l.clickLogin();
+		Home h=new Home(driver);
+		h.clickOnLogout();
+
+	}
+}
